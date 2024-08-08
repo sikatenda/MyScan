6f862e836b00540a0eb3e9fa544395437a86e9fb
Jacques Feruzi <jferuzi019@gmail.com>
2024-08-08 14:28:15 +0200

    banner
    removing the debug banner

--- SCAN/scan1/lib/main.dart
+++ SCAN/scan1/lib/main.dart
@@ -10,17 +10,16 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text("FERUZI"),
-          ),
-          backgroundColor: Colors.cyan[300],
+          title: const Text("SCAN QR"),
+          //backgroundColor: Colors.cyan[300],
         ),
         body: const Center(
           child: Text("Hello world!"),
         ),
-        backgroundColor: Colors.grey[400],
+        //backgroundColor: Colors.grey[400],
       ),
     );
   }
